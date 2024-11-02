c33489e18559a4eb3405b8c47768a2b14b6fc9f6
fransgtg <fransmalindoginting@gmail.com>
2024-11-02 20:34:53 +0700

    Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,21 @@
-//ketik kode jawaban noo 3 kamu disini
+uses crt; // untuk clear screen
+var n,x,y : integer; // 'x' sebagai batas awal pada baris matriks, 'y' batas awal pada kolom matriks, 'n' untuk input nilai 
+begin // memulai program
+    clrscr; // clear screen
+    write('Masukkan nilai n: '); readln(n); // memasukkan nilai 'n'
+    if n mod 2 = 0 then // menggunakan operasi kondisi untuk menginsialisasikan nilai 'n' genap
+    begin // memulai operasi kondisi
+    writeln('Input n harus ganjil'); // tampilan apabila diinputan 'n' genap
+    exit; // keluar dari blok program
+    end; // mengakhiri operasi kondisi
+    for x := 1 to n do // operasi perulangan dimana batas awal 'x' yaitu 1 dan batas akhir yaitu nilai 'n' (bagian baris)
+    begin // memulai operasi perulangan (bagin baris)
+        for y := 1 to n do // operasi perulangan dimana batas awal 'y' yaitu 1 dan batas akhir nilai 'n' (bagian kolom)
+        begin // memulai operasi perulangan (bagian kolom)
+            if (x = y) or (y = n - x + 1) then // operasi perulangan untuk mengatur posisi '*'
+            write('*') // menampilkan '*'
+            else write(' '); // apabila tidak sesuai dengan kondisi maka akan menampilkan spasi
+        end; // mengakhiri operasi perulangan (bagian kolom)
+        writeln; // untuk menampilkan baris baru saat program mengulang
+    end; // mengakhiri operasi perulangan (bagian baris)
+end. // mengakhiri program//ketik kode jawaban noo 3 kamu disini
